cc719c4cde5247600eca1e3ff07dc1576f8cf012
DoaaLa <personncreative56@gmail.com>
2023-08-14 23:45:13 +0000

    new commit '

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
