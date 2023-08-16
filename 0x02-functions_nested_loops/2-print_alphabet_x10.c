010435758236e508121fba854fc9c521119a52bd
DoaaLa <personncreative56@gmail.com>
2023-08-16 23:00:55 +0000

    new commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print text
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	j = 0;
+	while (j <= 10)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		j++;
+	}
+}
+
