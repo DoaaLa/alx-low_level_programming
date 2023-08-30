eb3b67b19f6dafcfa68aabb00ab3bf89611ca8e1
DoaaLa <personncreative56@gmail.com>
2023-08-30 21:31:44 +0000

    new commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to string
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
