5137b017d9646c0ff90c90b8581fd214abceb694
DoaaLa <personncreative56@gmail.com>
2023-09-05 23:28:33 +0000

    new commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,24 +9,22 @@
  */
 char *_strdup(char *str)
 {
-	char *new;
-	int i, n;
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 	for (n = 0; str[n] != '\0'; n++)
 		;
-	new = (char *)malloc(n * sizeof(char));
-	if (new != NULL)
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
 	{
 		for (i = 0; str[i] != '\0'; i++)
-			new[i] = str[i];
+			strnew[i] = str[i];
 	} else
 	{
-		return (NULL);
-	}
-	new[i] = '\0';
-	return (new);
+		return (NULL);													}
+	strnew[i] = '\0';
+	return (strnew);
 }
