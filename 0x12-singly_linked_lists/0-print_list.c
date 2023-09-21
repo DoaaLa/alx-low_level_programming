5ad4181807c2b08f7bca1f3b4a8873c68ad90072
DoaaLa <personncreative56@gmail.com>
2023-09-21 21:56:14 +0000

    new commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include <srdio.h>
+#include<stdlib.h>
+#include "list.h"
+
+/**
+ * print_list - prints the elements of a list
+ * @list_t: List we want to print
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+
+
+
