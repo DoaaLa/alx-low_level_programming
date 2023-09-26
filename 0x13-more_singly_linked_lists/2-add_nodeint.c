52fad2dee9d8e3e1953cbe1c9def1ae0701bb01d
DoaaLa <personncreative56@gmail.com>
2023-09-26 21:44:22 +0000

    new commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the begining of list
+ * @head: pointer to the head of the list
+ * @n: The integer of the new node
+ *
+ * Return: address of the new element on success
+ * On failure return NULL
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *tmp;
+
+	tmp = (listint_t *)malloc(sizeof(listint_t));
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		tmp->n = n;
+		tmp->next = head;
+		*head = tmp;
+	}
+	return (tmp);
+}
+
+
