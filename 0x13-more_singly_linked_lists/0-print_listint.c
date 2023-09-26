ea422676c6488acf1018d20bd17138e90846aa2e
DoaaLa <personncreative56@gmail.com>
2023-09-26 21:22:43 +0000

    new commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t c;
-	listint_t *tmp;
 
-	tmp = h;
 	c = 0;
-	while (tmp != NULL)
+	while (h)
 	{
 		c++;
-		printf("%d", tmp->n);
-		tmp = tmp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (c);
 }
